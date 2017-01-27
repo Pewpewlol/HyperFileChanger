14635e8cb04c4c50c9ab6895ce6b0d3aa3a2cdef
Pewpewlol <sandip.nijjar17@gmail.com>
2017-01-27 13:34:21 +0100

    Add files via upload

---
+++ MainViewModel/MainPageViewModel.cs
@@ -0,0 +1,41 @@
+﻿using XamlStandardsLib;
+using System.Threading.Tasks;
+using System;
+using Windows.System;
+using System.Windows;
+using Windows.UI.Popups;
+using Windows.UI;
+using HyperFileChanger.Class;
+using HyperFileChanger.Views;
+using Windows.UI.Xaml.Controls;
+using System.Diagnostics;
+
+namespace HyperFileChanger.MainViewModel
+{
+    public class MainPageViewModel : ModelBase
+    {
+        public FileModel Files { get; set; }
+        public DelegateCommand<string> DateiNameCommand { get; set; }
+        
+        public MainPageViewModel()
+        {
+            
+            Files = new FileModel();
+
+            this.DateiNameCommand = new DelegateCommand<string>
+            (
+                
+                async (s) => await Files.PickFile(),
+                (s) => true
+            );
+            
+
+
+
+        }
+    }
+   
+    
+    
+    
+}
